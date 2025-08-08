a3133d122c5baab1577a289db63ba931c39a2bfa
Rick <rick.yh.yang@gmail.com>
2025-08-07 23:17:17 -0400

    folder updates

--- test-repo/tests/test_iris.py
+++
@@ -1,5 +0,0 @@
-def test_iris_functionality():
-    from iris.iris import your_function  # Replace 'your_function' with the actual function you want to test
-    assert your_function() == expected_value  # Replace 'expected_value' with the expected output of your_function
-
-test_iris_functionality()
